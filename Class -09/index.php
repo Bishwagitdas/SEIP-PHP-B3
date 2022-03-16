7d03b41aad166ba317d6d99ee23aa440f2797947
bishwagitdas <bishwagitdas0@gmail.com>
2022-03-16 13:07:15 +0600

    php core

---
+++ Class -09/index.php
@@ -0,0 +1,40 @@
+<?php ?>
+<html>
+<head>
+    
+    <title>Learn php</title>
+<style>
+  .header{
+      background-color: green;
+      color:white;
+      text-aline: center;
+      padding: 20px;
+  }
+  .content{
+      background-color: black;
+      color: orange;
+      padding:100px;
+  }
+  .footer{
+      background-color: blue;
+      color: green ;
+      text-aline: center;
+      padding: 10px;
+  }
+</style>
+</head>
+<body>
+   <div class="header">
+       <?php echo "header" ?>
+
+   </div>
+   <div class="content">
+       <?php echo "content" ?>
+
+   </div>
+   <div class="footer">
+       <?php echo "footer" ?>
+
+   </div>
+</body>
+</html>
