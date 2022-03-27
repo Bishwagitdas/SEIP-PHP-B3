0b1fa9a873eeb8bfb9c37a2e3a0a085606caf1b9
bishwagitdas <bishwagitdas0@gmail.com>
2022-03-27 13:15:23 +0600

    class task

---
+++ Class -14/register.php
@@ -0,0 +1,16 @@
+<?php  
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+// $username =$_POST['username'];
+// $email =$_POST['email'];
+// $password =$_POST['password'];
+// $password2 =$_POST['password2'];
+
+// echo $username . "<br>";
+// echo $email . "<br";
+
+
+
+
+?>
---
+++ Class -15/index.php
@@ -0,0 +1,85 @@
+<?php  
+
+// $number= 1;
+// while($number<=10){
+    // if($number % 2 ==0){
+    //     $number ++;
+    //     continue;
+    // }
+
+    // if($number == 5){
+    //     break;
+    // }
+     
+//     echo "<p> $number </p>";
+//     $number ++;
+// }
+
+// $person = [
+//     'name' => 'nishan',
+//     'skills' => [
+//         'HTML' => [
+//             'version' => [4,5]
+//         ]
+//     ]
+//  ];
+
+//  echo "<pre>";
+
+//  print_r($person ['skills']['HTML']['version'][0]);
+//  print_r($person ['skills']['HTML']['version'][1]);
+
+//  echo "</pre>";
+
+#task_01:
+
+// $numbers = [1,2,3,4,5,6,7,8,9,10];
+
+//  echo "<pre";
+// foreach($numbers as $single_number){
+//     if($single_number % 2 == 1)
+//     {
+//         continue;
+//     }
+
+//     echo "$single_number" . "<br>";
+// }
+
+#task_02:
+
+// $personal_information = [
+//     'name' => 'Bishwagit',
+//     'email' =>  'bishwagitdas0@gmail.com',
+//     'address' => 'Dhaka'
+// ];
+
+// echo "<pre>";
+
+// foreach($personal_information as $key => $value){
+//     echo "$key = $value" . "<br>";
+// }
+
+
+#task_03:
+$subjects = [
+    'cse' => ['c','c++','java'],
+    'bba' => ['accounting', 'management','marketing'],
+    'eee' => ['basic-electronics']
+
+];
+
+echo "<pre>";
+
+// foreach($subjects as $key => $value){
+//    echo " <b style ='color:red'>$key .</b>";
+//    if(is_array($value)){
+//        foreach($value as $data){
+//            echo " &nbsp; &nbsp; &nbsp; $data .<br>";
+//        }
+//    }
+   
+// }
+
+echo "</pre>";
+
+?>
