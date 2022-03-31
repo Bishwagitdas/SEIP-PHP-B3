371d171be05d1a5025497e9502a9a96e8a183573
bishwagitdas <bishwagitdas0@gmail.com>
2022-03-31 12:31:02 +0600

    db login & registration

---
+++ Class -19/function/register.php
@@ -0,0 +1,31 @@
+<?php include('../db.php')?>
+
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+
+$insert_query = "INSERT INTO users (name,email,password)
+VALUES('$name','$email','$password')";
+
+$statment = $pdo->prepare($insert_query);
+$statment->execute();
+
+header("location: ../index.php");
+
+
+
+}
+else{
+
+    echo "Registration failed";
+
+}
+
+?>
